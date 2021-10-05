abbd57fd374709e86d6db50bf6870b9d3fcbf7a6
zpuckettdesign <zpuckettdesign@gmail.com>
2021-10-05 15:13:36 -0500

    hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+      Welcome. <br />
+      Feel Free to Look Around!
+      </SectionTitle>
+      <SectionText>
+        My name is Zach Puckett, and my mission is to impact the world of technology with effective design, clear and concise communication and expert engineering.
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
